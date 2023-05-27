927562f9d354ee003237779f05fca6fdca379e05
BushyOn <bushybiscoito@gmail.com>
2023-05-27 16:34:37 -0300

    Initial script code

---
+++ sink_change.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Rotates between sink devices (sound output devices).
+rotate_sound_cards() {
+    active_sink=$(pactl get-default-sink)
+    sinks=$(pactl list short sinks | grep -v "hdmi" | awk '{print $2}')
+    
+    sinks=$(pactl list short sinks | awk '{print $2}')
+    if [[ -z $active_sink || -z $sinks ]]; then
+        echo "Unable to retrieve sound cards or HDMI sinks"
+        exit 1
+    fi
+    found_active_sink=false
+    next_sink=""
+
+    for sink in $sinks; do
+        if [[ $sink != $active_sink ]]; then
+            pactl set-default-sink "$sink"
+            echo "Rotated sound card: $active_sink to $sink"
+            break
+        fi
+    done
+}
+
+# This is a customized part, change the strings as you need. 
+device(){
+    active_device=$(pactl get-default-sink)
+    
+    if [[ $active_device == *"Focusrite"* ]]; then
+        echo Headphone
+    elif [[ $active_device == *"pci-0000_00_1f"* ]]; then 
+        echo Speaker
+    else 
+        echo Not Configured
+    fi
+}
+
+if [ $1 = "--change" ]; then
+    rotate_sound_cards
+elif [ $1 = "--device" ]; then
+    device
+fi
