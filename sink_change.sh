4b27d3699e03100c7ce2dd17bef4d193b7472824
Bushy <43689053+Bushyon@users.noreply.github.com>
2024-01-27 11:23:10 -0300

    Added HDMI and multiple sinks support

--- sink_change.sh
+++ sink_change.sh
@@ -3,19 +3,40 @@
 # Rotates between sink devices (sound output devices).
 rotate_sound_cards() {
     active_sink=$(pactl get-default-sink)
-    sinks=$(pactl list short sinks | grep -v "hdmi" | awk '{print $2}')
-    
+    sinks=$(pactl list short sinks | awk '{print $2}')
+
     if [[ -z $active_sink || -z $sinks ]]; then
         echo "Unable to retrieve sound cards or HDMI sinks"
         exit 1
     fi
+
+    # Set numeric IDs for each sink
+    declare -A sink_ids
+    index=0
     for sink in $sinks; do
-        if [[ $sink != $active_sink ]]; then
-            pactl set-default-sink "$sink"
-            echo "Rotated sound card: $active_sink to $sink"
+        sink_ids["$sink"]=$index
+        ((index++))
+    done
+
+    # Get ID of the active sink
+    active_sink_id=${sink_ids["$active_sink"]}
+
+    # Calculate the next ID, wrapping around if necessary
+    next_sink_id=$(( (active_sink_id + 1) % index ))
+
+    # Get the sink name with the next ID
+    next_sink=""
+    for sink in "${!sink_ids[@]}"; do
+        if [[ ${sink_ids["$sink"]} -eq $next_sink_id ]]; then
+            next_sink="$sink"
             break
         fi
     done
+
+    # Set the next sink as the default
+    pactl set-default-sink "$next_sink"
+
+    echo "Rotated sound card: $active_sink to $next_sink"
 }
 
 # This is a customized part, change the strings as you need. 
@@ -37,3 +58,4 @@ if [ $1 = "--change" ]; then
 elif [ $1 = "--device" ]; then
     device
 fi
+
